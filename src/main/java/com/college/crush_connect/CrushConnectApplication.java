d4bb9bd5e3fe84411bdf7e496ddc3e3ee010294b
Sucheth <ksucheth03@gmail.com>
2026-02-07 19:48:56 +0530

    Initial commit

---
+++ src/main/java/com/college/crush_connect/CrushConnectApplication.java
@@ -0,0 +1,13 @@
+package com.college.crush_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrushConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrushConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/crush_connect/CrushConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.crush_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrushConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
